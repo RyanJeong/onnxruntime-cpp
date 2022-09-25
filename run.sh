8c95067fdc3c039ffdcb6c600cbbd1ca20def6d6
RyanJeong <wat6988@gmail.com>
2022-09-25 19:11:50 +0900

    fix: Fixed run.sh and tool.cmake

--- run.sh
+++ run.sh
@@ -6,14 +6,7 @@
 #     (2) linaro-aarch64-2020.09-gcc10.2-linux5.4
 
 NUM_CORE=$(grep processor /proc/cpuinfo | awk '{field=$NF};END{print field+1}')
-PROTOC_DIR=/usr/local/protoc-aarch64
-ONNX_TOOLCHAIN=toolchain.cmake
 WORKING_DIR=$(pwd)
-
-cd $WORKING_DIR/
-
-sudo mkdir -p $ONNX_DEPENDENCIES/
-sudo chmod 777 -R $ONNX_DEPENDENCIES/
 
 # 1. protobuf
 cd $WORKING_DIR
@@ -25,39 +18,34 @@ ONNX_PROTOBUF="protobuf-\
 $ONNX_PROTOBUF_VERSION_MAJOR"."\
 $ONNX_PROTOBUF_VERSION_MINOR"."\
 $ONNX_PROTOBUF_VERSION_PATCH"
-ONNX_PROTOBUF_URL="https://github.com/protocolbuffers/protobuf/archive/refs/tags/v\
+ONNX_PROTOBUF_URL="https://github.com/protocolbuffers/protobuf/releases/download/v3.18.1/protoc-\
 $ONNX_PROTOBUF_VERSION_MAJOR"."\
 $ONNX_PROTOBUF_VERSION_MINOR"."\
-$ONNX_PROTOBUF_VERSION_PATCH".tar.gz
+$ONNX_PROTOBUF_VERSION_PATCH"-linux-x86_64.zip
 
 wget $ONNX_PROTOBUF_URL
-tar -xvf $WORKING_DIR/v$ONNX_PROTOBUF_VERSION_MAJOR.$ONNX_PROTOBUF_VERSION_MINOR.$ONNX_PROTOBUF_VERSION_PATCH.tar.gz
-rm *.tar.gz
-cd $WORKING_DIR/$ONNX_PROTOBUF/
+unzip $WORKING_DIR/protoc-$ONNX_PROTOBUF_VERSION_MAJOR.$ONNX_PROTOBUF_VERSION_MINOR.$ONNX_PROTOBUF_VERSION_PATCH-linux-x86_64.zip -d $WORKING_DIR/$ONNX_PROTOBUF
+rm *.zip
+
+# 2. [WIP] onnxruntime
+cd $WORKING_DIR
+
+pip uninstall onnx
+
+git clone git@github.com:microsoft/onnxruntime.git
+cd onnxruntime
+# must match the version of the protoc which is used in onnxruntime that currently using
+git checkout v1.12.0
+git submodule sync
+git submodule update --init --recursive
+
 mkdir aarch64_build
 cd aarch64_build/
+
 cmake ../cmake -G"Unix Makefiles" \
-  -DCMAKE_TOOLCHAIN_FILE=$WORKING_DIR/$ONNX_TOOLCHAIN \
-  -DCMAKE_INSTALL_PREFIX=$ONNX_DEPENDENCIES \
-  -DCMAKE_BUILD_TYPE=Release \
-  -Dprotobuf_BUILD_TESTS=OFF
+  -DONNX_CUSTOM_PROTOC_EXECUTABLE=$WORKING_DIR/$ONNX_PROTOUF/bin/protoc \
+  -DCMAKE_TOOLCHAIN_FILE=$WORKING_DIR/tool.cmake
 cmake --build . \
   --config Release \
   --target install \
   -- -j$NUM_CORE
-export PATH=$PATH:$ONNX_DEPENDENCIES/bin
-
-# 2. [WIP]onnxruntime
-cd $WORKING_DIR
-
-git clone git@github.com:microsoft/onnxruntime.git
-cd onnxruntime
-# to sync the version(3.18.1) with protobuf
-git checkout v1.12.1
-git submodule sync
-git submodule update --init --recursive
-
-cp $WORKING_DIR/tool.cmake ./
-
-cmake -DONNX_CUSTOM_PROTOC_EXECUTABLE=$PROTOC_DIR/bin \
-  -DCMAKE_TOOLCHAIN_FILE=tool.cmake
