d1bbdf052913c12f01bd3822992fdc611aae6b42
RyanJeong <wat6988@gmail.com>
2022-09-26 19:02:14 +0900

    fix: Fixed typo

--- raspi_run.sh
+++ raspi_run.sh
@@ -20,31 +20,34 @@ pip3 install --upgrade wheel
 pip3 install numpy
 
 # Build the latest cmake
-mkdir /code
-cd /code
+mkdir code
+cd code
 wget https://cmake.org/files/v3.16/cmake-3.16.1.tar.gz;
 tar zxf cmake-3.16.1.tar.gz
 
-cd /code/cmake-3.16.1
+cd cmake-3.16.1
 ./configure --system-curl
 # make -j$NUM_CORE
 make -j2
 sudo make install
 
 # Prepare onnxruntime Repo
-cd /code
+cd ~/code
 git clone --recursive https://github.com/Microsoft/onnxruntime
 
 # Start the basic build
-cd /code/onnxruntime
-./build.sh --config MinSizeRel --update --build
+cd onnxruntime
+# ./build.sh --config MinSizeRel --update --build
+./build.sh --config Release --update --build
 
 # Build Shared Library
-./build.sh --config MinSizeRel --build_shared_lib
+# ./build.sh --config MinSizeRel --build_shared_lib
+./build.sh --config Release --build_shared_lib
 
 # Build Python Bindings and Wheel
-./build.sh --config MinSizeRel --enable_pybind --build_wheel
+# ./build.sh --config MinSizeRel --enable_pybind --build_wheel
+./build.sh --config Release --enable_pybind --build_wheel
 
 # Build Output
-ls -l /code/onnxruntime/build/Linux/MinSizeRel/*.so
-ls -l /code/onnxruntime/build/Linux/MinSizeRel/dist/*.whl
+# ls -l /code/onnxruntime/build/Linux/MinSizeRel/*.so
+# ls -l /code/onnxruntime/build/Linux/MinSizeRel/dist/*.whl
