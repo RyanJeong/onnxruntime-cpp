c161bd4cb8a2ec3ab5fae6fe0130217d24bbb048
RyanJeong <msjeong@cochlear.ai>
2022-09-27 17:00:12 +0900

    fix: Removed unnecessary files

--- raspi_docker.sh
+++
@@ -1,9 +0,0 @@
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
-git clone git@github.com:microsoft/onnxruntime.git
-cd onnxruntime
-git submodule sync
-git submodule update --init --recursive
-cd dockerfiles
-docker build -t onnxruntime-source -f Dockerfile.arm64 ..
-docker run -it onnxruntime-source
