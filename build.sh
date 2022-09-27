594b5e6af43c6a685af540dfbbd75fca677a619d
RyanJeong <msjeong@cochlear.ai>
2022-09-27 18:22:01 +0900

    fix: Fixed to use the repo as submodule

---
+++ build.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+NUM_CORE=$(grep processor /proc/cpuinfo | awk '{field=$NF};END{print field+1}')
+WORKING_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
+
+# protobuf
+PROTOBUF_VERSION_MAJOR=3
+PROTOBUF_VERSION_MINOR=18
+PROTOBUF_VERSION_PATCH=1
+PROTOBUF="protobuf-\
+$PROTOBUF_VERSION_MAJOR"."\
+$PROTOBUF_VERSION_MINOR"."\
+$PROTOBUF_VERSION_PATCH"
+PROTOBUF_FILENAME=$WORKING_DIR/$PROTOBUF.zip
+PROTOBUF_FOLDER=$WORKING_DIR/$PROTOBUF
+PROTOBUF_URL="https://github.com/protocolbuffers/protobuf/releases/download/v\
+$PROTOBUF_VERSION_MAJOR"."\
+$PROTOBUF_VERSION_MINOR"."\
+$PROTOBUF_VERSION_PATCH"/protoc-"\
+$PROTOBUF_VERSION_MAJOR"."\
+$PROTOBUF_VERSION_MINOR"."\
+$PROTOBUF_VERSION_PATCH"-linux-x86_64.zip
+
+if [ ! -f "$PROTOBUF_FILENAME" ]; then
+  wget -q $PROTOBUF_URL -O $PROTOBUF_FILENAME
+fi
+
+if [ ! -d $PROTOBUF_FOLDER ]; then
+  mkdir -p $PROTOBUF_FOLDER
+  unzip -q $PROTOBUF_FILENAME -d $PROTOBUF_FOLDER
+fi
+
+# onnxruntime
+ONNXRUNTIME_FOLDER=$WORKING_DIR/onnxruntime
+ONNXRUNTIME_VERSION=v1.12.0
+if [ ! -d $ONNXRUNTIME_FOLDER ]; then
+  git clone git@github.com:microsoft/onnxruntime.git -b $ONNXRUNTIME_VERSION
+fi
+cd $ONNXRUNTIME_FOLDER
+git pull origin $ONNXRUNTIME_VERSION
+git submodule sync
+git submodule update --init --recursive
+
+# error: def process_ifs(lines: Iterable[str], onnx_ml: bool) -> Iterable[str]:
+# You MUST explicitly modify the Python version you are using for your environment to avoid this error
+# 
+# $ python3 --version
+# Python 3.6.9
+cp $WORKING_DIR/onnx_hotfix $ONNXRUNTIME_FOLDER/cmake/external/onnx/CMakeLists.txt
+
+ONNXRUNTIME_BUILD=$ONNXRUNTIME_FOLDER/onnxruntime_build
+# remove all configurations are used before
+if [ -d $ONNXRUNTIME_BUILD ]; then
+  rm -rf $ONNXRUNTIME_BUILD
+fi
+mkdir -p $ONNXRUNTIME_BUILD
+cd $ONNXRUNTIME_BUILD
+cmake ../cmake -G"Unix Makefiles" \
+  -DCMAKE_INSTALL_PREFIX=$WORKING_DIR \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DONNX_CUSTOM_PROTOC_EXECUTABLE=$PROTOBUF_FOLDER/bin/protoc \
+  -DCMAKE_TOOLCHAIN_FILE=$WORKING_DIR/tool.cmake
+cmake --build . \
+  --config Release \
+  --target install \
+  -- -j$NUM_CORE
+
