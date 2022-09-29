ce3d9481c47f13c595c4510da3559017fc22c80a
RyanJeong <wat6988@gmail.com>
2022-09-29 22:39:50 +0900

    fix: Fixed tool.cmake

--- tool.cmake
+++ tool.cmake
@@ -4,3 +4,7 @@ set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+set(CMAKE_C_COMPILER /usr/local/linaro-aarch64-2020.09-gcc10.2-linux5.4/bin/aarch64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER /usr/local/linaro-aarch64-2020.09-gcc10.2-linux5.4/bin/aarch64-linux-gnu-g++)
