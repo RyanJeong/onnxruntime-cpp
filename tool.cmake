653756783c0c86aed1b543d04198965d40012d7f
RyanJeong <wat6988@gmail.com>
2022-10-02 13:58:32 +0900

    fix: Fixed tool.cmake

--- tool.cmake
+++ tool.cmake
@@ -10,4 +10,5 @@ set(CMAKE_SYSTEM_PROCESSOR aarch64)
 set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc)
 set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++)
 # set(CMAKE_C_COMPILER /usr/local/linaro-aarch64-2020.09-gcc10.2-linux5.4/bin/aarch64-linux-gnu-gcc)
-# set(CMAKE_CXX_COMPILER /usr/local/linaro-aarch64-2020.09-gcc10.2-linux5.4/bin/aarch64-linux-gnu-g++)
+# set(CMAKE_CXX_COMPILER /usr/local/linaro-aarch64-2020.09-gcc10.2-linux5.4/bin/aarch64-linux-gnu-g++)set(CMAKE_SYSTEM_VERSION 1)
+
